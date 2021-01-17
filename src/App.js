ede1124d2d2570052ff7b5c1eb05643df0cbe5a8
Khaled Ellabban <m.khaled_ellabban@hotmial.com>
2021-01-17 08:37:23 +0200

    some Routing using react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage.component';
 
+const HatsPage = (props) => (
+  <div>
+    {    console.log(props)}
+    <h1> Hats Page </h1>
+  </div>
+)
+
+const JacketPage = (props) => {
+  return(
+    <div>
+      <h1> Jacket Pages </h1>
+      <Link to='/jacket/15'> Jacket Id </Link>
+      <button onClick={ () => props.history.push('/hats') }>Homepage</button>
+      <Link to={ `${props.match.url}/21`}> topic 21 </Link>
+      <Link to={ `${props.match.url}/15`}> topic 15 </Link>
+      <Link to={ `${props.match.url}/13`}> topic 13 </Link>
+    </div>
+  );
+};
+const JacketDetails = (props) => {
+  console.log(props);
+  return(
+    <div>
+      <h1> JacketDetails Page: { props.match.params.jacketId } </h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage  />
+      
+        <Route exact  path='/' component={HomePage}  />
+        <Route exact  path='/hats' component={HatsPage}  />
+        <Route exact  path='/jacket' component={JacketPage}  />
+        <Route exact  path='/jacket/:jacketId' component={JacketDetails}  />
+      
     </div>
   );
 }
