605bcaae7e7d7bf20533f687856d0f7037c91ee0
Khaled Ellabban <m.khaled_ellabban@hotmial.com>
2021-02-17 23:14:29 +0200

    create new selectCollectionForPerview to by converting object to array to map over them

--- src/components/collections-overview/collections-overview.componet.jsx
+++ src/components/collections-overview/collections-overview.componet.jsx
@@ -2,24 +2,27 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selector';
+import { selectCollectionForPreview } from '../../redux/shop/shop.selector';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
 import './collections-overview.styles.scss';
 
-const CollectionsOverview = ({ collections }) => (
+const CollectionsOverview = ({ collections }) => {
+    // console.log(collections); // for converting the object to array to make map function work
+    return(
     <div className='collections'>
         {
             collections.map( ({id, ...otherCollectionPreview}) => (
                 <CollectionPreview  key={id} {...otherCollectionPreview} />
             ))
+            
         }
     </div>
-);
+)};
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
